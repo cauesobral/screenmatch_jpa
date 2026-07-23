7a7c2edfa25de9e77ef35d1a5e708147a14fb560
Cauê Sobral <caueroberto56@gmail.com>
2026-07-23 15:55:22 -0300

    criação da classe SerieRepository

---
+++ screenmatch-jpa/src/main/java/br/com/cauesobral/screenmatch/repository/SerieRepository.java
@@ -0,0 +1,15 @@
+package br.com.cauesobral.screenmatch.repository;
+
+import br.com.cauesobral.screenmatch.model.Categoria;
+import br.com.cauesobral.screenmatch.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, double avaliacao);
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+    List<Serie> findByGenero(Categoria categoria);
+    List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
+}
